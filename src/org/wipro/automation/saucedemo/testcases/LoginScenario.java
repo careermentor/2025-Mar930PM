ea4af5da63713e7999993c1e2a7db36753326334
Santosh Modi <modisantosh@gmail.com>
2025-04-15 22:11:20 +0530

    updated code

--- src/org/wipro/automation/saucedemo/testcases/LoginScenario.java
+++ src/org/wipro/automation/saucedemo/testcases/LoginScenario.java
@@ -5,29 +5,23 @@ import org.wipro.automation.saucedemo.basepkg.InitiateBrowser;
 import org.wipro.automation.saucedemo.pages.LoginPage_PF;
 import org.wipro.automation.saucedemo.utilitiespkg.ReadPropFilesData;
 
-public class LoginScenario extends InitiateBrowser
-{
+public class LoginScenario extends InitiateBrowser {
 
 	@Test
-	
-	
-	
-	public void tc01_login_valid_credentials() throws Throwable
-	{
-	
+	public void tc01_login_valid_credentials() throws Throwable {
+
 		LoginPage_PF login = new LoginPage_PF(driver);
-		
-		//because "this.driver" is null
-		
-		//login.username_textbox("standard_user");
-		//login.password_textbox("secret_sauce");
-		
+
+		// because "this.driver" is null
+
+		// login.username_textbox("standard_user");
+		// login.password_textbox("secret_sauce");
+
 		login.username_textbox(ReadPropFilesData.readconfigData("username"));
 		login.password_textbox(ReadPropFilesData.readconfigData("password"));
-	
-		
+
 		login.loginbttn();
-		
+
 	}
-	
+
 }
